f5b45be0f67a66558c0744f15ac70a51369bc7a4
Erick Levy <ericklevy@gmail.com>
2021-10-08 19:55:41 -0500

    Initial functional commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HeaderComponent } from './components/header/header.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { MaterialModule } from './shared/app.modules';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokeTableComponent,
+    PokeDetailComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
